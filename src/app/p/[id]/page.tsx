a182854b0a2fe378e970362de175c7cd2f56a78a
Mbdulrohim <doyextech@gmail.com>
2026-02-11 23:04:46 +0100

    feat: Set runtime to 'edge' for ProjectPage component

--- src/app/p/[id]/page.tsx
+++ src/app/p/[id]/page.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import { Workspace } from "@/components/Workspace";
 import { useParams } from "next/navigation";
 
+export const runtime = 'edge';
+
 const ProjectPage: React.FC = () => {
   const params = useParams();
   const id = Array.isArray(params.id) ? params.id[0] : params.id;
